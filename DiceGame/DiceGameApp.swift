171e1a1be3f6cc047fbcd7993673f57ad090504f
Navi9519 <dedicivanovic@gmail.com>
2024-09-24 09:07:25 +0200

    Initial Commit

---
+++ DiceGame/DiceGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceGameApp.swift
+//  DiceGame
+//
+//  Created by Ivan Dedic on 2024-09-24.
+//
+
+import SwiftUI
+
+@main
+struct DiceGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
